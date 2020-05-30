714dc2a95ac18c25d6ae9f4c55ef91cbe4a1d7aa
Barbs <barbarap095@gmail.com>
2020-05-30 15:19:44 +1000

    Commit changes

--- Develop/script.js
+++ Develop/script.js
@@ -8,47 +8,60 @@ function getRandomNumber(randNumber) {
 }
 
 // Generate length of password
-var length = function() {
-    return prompt("How long do you want your password to be?"); 
-};
-   
-
-// Create input variables for passwords
-var letter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-var special = ['!','@', '&', '$', '%', '<', '^', '<', '~', '#', '}', ',', '|', '{', '`', '_', '”', '(', ')', '*', '+'];
-
-// Prompt length of password
 var setLength = prompt("How long do you want your password to be?");
 
-if (setLength < 8 || setLength > 128) {
-    alert("Your password must be between 8 and 128 characters long. Please try again.");
-} else {
+if (setLength >= 8 && setLength <= 128) {
     console.log(setLength);
+} 
+
+else {
+    alert("Your password must be between 8 and 128 characters long. Please try again.");
+    setLength.value = 8;
+    console.log(setLength.value);
 }
 
-// Prompt for special characters
-var specialChar = confirm("Would you like to include special characters in your password?");
+// Create input variables for passwords
+    var letterChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    var specialChar = ['!','@', '&', '$', '%', '<', '^', '<', '~', '#', '}', ',', '|', '{', '`', '_', '”', '(', ')', '*', '+'];
 
-// Prompt for lowercase characters
-var lowerChar = confirm("Would you like to include lower case letters in your password?")
 
-// Prompt for uppercase characters
-var upperChar = confirm("Would you like to include upper case letters in your password?")
+    // Confirm special characters
+    var confirmSpecial = confirm("Would you like to include special characters in your password?");
+        console.log(confirmSpecial);
 
-// Generate password 
+    // Prompt number of special characters
+        if (confirmSpecial === true) {
+            var countSpecial = prompt("How many special characters would you like to include?");
+            console.log(countSpecial); 
+                
+            // Specify random number of characters
+            var randomSpecial = [specialChar[getRandomNumber(countSpecial.value)]];
+            console.log(randomSpecial);
 
-function generateChar() {
+        }
+        // Prompt for lowercase characters
+        var confirmLower = confirm("Would you like to include lower case letters in your password?");
+            console.log(confirmLower);
+            if (confirmLower === true) 
 
-    //Retrieve value of setLength
-    let length = document.prompt(setLength).value;
-    
-
-}
-
-document.getElementById("password").innerHTML = setLength;
+        // Prompt for uppercase characters
+        var upperChar = confirm("Would you like to include upper case letters in your password?");
+            console.log(upperChar);
 
 
 //  Validate input so that at least one character type is selected
 
-// Once all prompts are answered, generate password through an alert OR written on page
+// Once all prompts are answered, generate password 
+
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
+  
+  // Add event listener to generate button
+  generatePassword.addEventListener("click", writePassword);
